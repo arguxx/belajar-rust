e8929550558902f25a57e95e45c9e8a417060651
arguxx <argianradityaputra21@gmail.com>
2024-11-10 13:30:47 +0700

    unit test

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,18 @@ fn main() {
     print!("kuda");
     print!("-kuda");
 }
+
+#[test]
+fn func_kedua() {
+    print!("kuda");
+    print!("-kuda");
+    println!("-kuda");
+}
+
+#[test]
+fn func_ketiga() {
+    print!("kuda");
+    print!("-kuda");
+    println!("-kuda");
+    
+}
